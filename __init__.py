3a71bc0f72bc5f645947b8b751a79ce1d29a468c
sarra923 <83019279+sarra923@users.noreply.github.com>
2024-09-12 16:18:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -33,6 +33,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def histogramme():
     return render_template("histogramme.html")
+
+@app.route("/commits/")
+def commits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
